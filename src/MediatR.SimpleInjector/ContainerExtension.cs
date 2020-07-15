52cea4c1d4ecf70f7aa04a105c6680a5c5880ff1
jacob.phan <jacob.phan@starixsoft.com>
2020-07-15 10:46:38 +0700

    Upgrade SimpleInjector 5.0.3

--- src/MediatR.SimpleInjector/ContainerExtension.cs
+++ src/MediatR.SimpleInjector/ContainerExtension.cs
@@ -15,26 +15,48 @@ namespace MediatR.SimpleInjector {
         }
 
         public static Container BuildMediator (this Container container, IEnumerable<Assembly> assemblies) {
-            var allAssemblies = new List<Assembly> { typeof (IMediator).GetTypeInfo ().Assembly };
-            allAssemblies.AddRange (assemblies);
+            var allAssemblies = GetAssemblies(assemblies);
 
-            container.RegisterSingleton<IMediator, Mediator> ();
-            container.Register (typeof (IRequestHandler<,>), allAssemblies);
+            container.RegisterSingleton<IMediator, Mediator>();
+            container.Register(typeof(IRequestHandler<,>), assemblies);
 
-            // we have to do this because by default, generic type definitions (such as the Constrained Notification Handler) won't be registered
-            var notificationHandlerTypes = container.GetTypesToRegister (typeof (INotificationHandler<>), assemblies, new TypesToRegisterOptions {
-                IncludeGenericTypeDefinitions = true,
-                    IncludeComposites = false,
+            RegisterHandlers(container, typeof(INotificationHandler<>), allAssemblies);
+            RegisterHandlers(container, typeof(IRequestExceptionAction<,>), allAssemblies);
+            RegisterHandlers(container, typeof(IRequestExceptionHandler<,,>), allAssemblies);
+
+            //Pipeline
+            container.Collection.Register(typeof(IPipelineBehavior<,>), new []
+            {
+                typeof(RequestExceptionProcessorBehavior<,>),
+                typeof(RequestExceptionActionProcessorBehavior<,>),
+                typeof(RequestPreProcessorBehavior<,>),
+                typeof(RequestPostProcessorBehavior<,>)
             });
-            container.Register (typeof (INotificationHandler<>), notificationHandlerTypes);
 
-            container.Collection.Register (typeof (IPipelineBehavior<,>), Enumerable.Empty<Type> ());
-            container.Collection.Register (typeof (IRequestPreProcessor<>), Enumerable.Empty<Type> ());
-            container.Collection.Register (typeof (IRequestPostProcessor<,>), Enumerable.Empty<Type> ());
-
-            container.Register (() => new ServiceFactory (container.GetInstance), Lifestyle.Singleton);
+            container.Register(() => new ServiceFactory(container.GetInstance), Lifestyle.Singleton);
 
             return container;
         }
+
+        private static void RegisterHandlers(Container container, Type collectionType, Assembly[] assemblies)
+        {
+            // we have to do this because by default, generic type definitions (such as the Constrained Notification Handler) won't be registered
+            var handlerTypes = container.GetTypesToRegister(collectionType, assemblies, new TypesToRegisterOptions
+            {
+                IncludeGenericTypeDefinitions = true,
+                IncludeComposites = false,
+            });
+
+            container.Collection.Register(collectionType, handlerTypes);
+        }
+
+        private static Assembly[] GetAssemblies(IEnumerable<Assembly> assemblies)
+        {
+            var allAssemblies = new List<Assembly> { typeof (IMediator).GetTypeInfo ().Assembly };
+            allAssemblies.AddRange (assemblies);
+            
+            return allAssemblies.ToArray();            
+        }
+
     }
 }
